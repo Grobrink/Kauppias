145fe33864f9d4dfb49d952b6e469b701aad5979
Pierre Lefèvre <karapost@gmail.com>
2014-07-09 17:20:08 +0200

    Work on subraces

--- js/script.js
+++ js/script.js
@@ -16,8 +16,9 @@ $(function () {
 		name: '',
 		level: 0,
 		hitPoint: 0,
-		Alignement: '',
-		Race: '',
+		alignement: '',
+		race: '',
+		subrace: '',
 		language: [],
 		wealth: '',
 		treasure: 0,
@@ -28,9 +29,58 @@ $(function () {
 
 	var nameList = {
 		Human: {
-			male: ['Darvin', 'Dorn', 'Evendur', 'Gorstag', 'Grim', 'Helm', 'Malark', 'Morn', 'Randal', 'Stedd'],
-			female: ['Arveene', 'Esvele', 'Jhessail', 'Kerri', 'Lureene', 'Miri', 'Rowan', 'Shandri', 'Tessele'],
-			lastnames: ['Amblecrown', 'Buckman', 'Dundragon', 'Evenwood', 'Greycastle', 'Tallstag']
+			subraces: {
+				Calishite: {
+					male: ['Aseir', 'Bardeid', 'Haseid', 'Khemed', 'Mehmen', 'Sudeiman', 'Zasheir'],
+					female: ['Atala', 'Ceidil', 'Hama', 'Jasmal', 'Meilil', 'Seipora', 'Yasheira', 'Zasheida'],
+					lastnames: ['Basha', 'Dumein', 'Jassan', 'Khalid', 'Mostana', 'Pashar', 'Rein']
+				},
+				Chondathan: {
+					male: ['Darvin', 'Dorn', 'Evendur', 'Gorstag', 'Grim', 'Helm', 'Malark', 'Morn', 'Randal', 'Stedd'],
+					female: ['Arveene', 'Esvele', 'Jhessail', 'Kerri', 'Lureene', 'Miri', 'Rowan', 'Shandri', 'Tessele'],
+					lastnames: ['Amblecrown', 'Buckman', 'Dundragon', 'Evenwood', 'Greycastle', 'Tallstag']
+				},
+				Damaran: {
+					male: ['Bor', 'Fodel', 'Glar', 'Grigor', 'Igan', 'Ivor', 'Kosef', 'Mival', 'Orel', 'Pavel', 'Sergor'],
+					female: ['Alethra', 'Kara', 'Katernin', 'Mara', 'Natali', 'Olma', 'Tana', 'Zora'],
+					lastnames: ['Bersk', 'Chernin', 'Dotsk', 'Kulenov', 'Marsk', 'Nemetsk', 'Shemov', 'Starag']
+				},
+				Illuskan: {
+					male: ['Darvin', 'Dorn', 'Evendur', 'Gorstag', 'Grim', 'Helm', 'Malark', 'Morn', 'Randal', 'Stedd'],
+					female: ['Arveene', 'Esvele', 'Jhessail', 'Kerri', 'Lureene', 'Miri', 'Rowan', 'Shandri', 'Tessele'],
+					lastnames: ['Amblecrown', 'Buckman', 'Dundragon', 'Evenwood', 'Greycastle', 'Tallstag']
+				},
+				Mulan: {
+					male: ['Darvin', 'Dorn', 'Evendur', 'Gorstag', 'Grim', 'Helm', 'Malark', 'Morn', 'Randal', 'Stedd'],
+					female: ['Arveene', 'Esvele', 'Jhessail', 'Kerri', 'Lureene', 'Miri', 'Rowan', 'Shandri', 'Tessele'],
+					lastnames: ['Amblecrown', 'Buckman', 'Dundragon', 'Evenwood', 'Greycastle', 'Tallstag']
+				},
+				Rashemi: {
+					male: ['Darvin', 'Dorn', 'Evendur', 'Gorstag', 'Grim', 'Helm', 'Malark', 'Morn', 'Randal', 'Stedd'],
+					female: ['Arveene', 'Esvele', 'Jhessail', 'Kerri', 'Lureene', 'Miri', 'Rowan', 'Shandri', 'Tessele'],
+					lastnames: ['Amblecrown', 'Buckman', 'Dundragon', 'Evenwood', 'Greycastle', 'Tallstag']
+				},
+				Shou: {
+					male: ['Darvin', 'Dorn', 'Evendur', 'Gorstag', 'Grim', 'Helm', 'Malark', 'Morn', 'Randal', 'Stedd'],
+					female: ['Arveene', 'Esvele', 'Jhessail', 'Kerri', 'Lureene', 'Miri', 'Rowan', 'Shandri', 'Tessele'],
+					lastnames: ['Amblecrown', 'Buckman', 'Dundragon', 'Evenwood', 'Greycastle', 'Tallstag']
+				},
+				Tethyrian: {
+					male: ['Darvin', 'Dorn', 'Evendur', 'Gorstag', 'Grim', 'Helm', 'Malark', 'Morn', 'Randal', 'Stedd'],
+					female: ['Arveene', 'Esvele', 'Jhessail', 'Kerri', 'Lureene', 'Miri', 'Rowan', 'Shandri', 'Tessele'],
+					lastnames: ['Amblecrown', 'Buckman', 'Dundragon', 'Evenwood', 'Greycastle', 'Tallstag']
+				},
+				Turami: {
+					male: ['Darvin', 'Dorn', 'Evendur', 'Gorstag', 'Grim', 'Helm', 'Malark', 'Morn', 'Randal', 'Stedd'],
+					female: ['Arveene', 'Esvele', 'Jhessail', 'Kerri', 'Lureene', 'Miri', 'Rowan', 'Shandri', 'Tessele'],
+					lastnames: ['Amblecrown', 'Buckman', 'Dundragon', 'Evenwood', 'Greycastle', 'Tallstag']
+				}
+			}
+		},
+		Human2: {
+			male: ['Adrik', 'Alberich', 'Baern', 'Barendd', 'Brottor', 'Bruenor', 'Dain', 'Darrak', 'Delg', 'Eberk', 'Einkil', 'Fargrim', 'Flint', 'Gardain', 'Harbek', 'Kildrak', 'Morgran', 'Orsik', 'Oskar', 'Rangrim', 'Rurik', 'Taklinn', 'Thoradin', 'Thorin', 'Tordek', 'Traubon', 'Travok', 'Ulfgar', 'Veit', 'Vondal'],
+			female: ['Amber', 'Artin', 'Audhild', 'Bardryn', 'Dagnal', 'Diesa', 'Eldeth', 'Falkrunn', 'Finellen', 'Gunnloda', 'Gurdis', 'Helja', 'Hlin', 'Kathra', 'Kristryd', 'Ilde', 'Liftrasa', 'Mardred', 'Riswynn', 'Sannl', 'Torbera', 'Torgga', 'Vistra'],
+			lastnames: ['Balderk', 'Battlehammer', 'Brawnanvil', 'Dankil', 'Fireforge', 'Frostbeard', 'Gorunn', 'Holderhek', 'Ironfist', 'Loderr', 'Lutgehr', 'Rumnaheim', 'Strakeln', 'Torunn', 'Ungart']
 		},
 		Dwarf: {
 			male: ['Adrik', 'Alberich', 'Baern', 'Barendd', 'Brottor', 'Bruenor', 'Dain', 'Darrak', 'Delg', 'Eberk', 'Einkil', 'Fargrim', 'Flint', 'Gardain', 'Harbek', 'Kildrak', 'Morgran', 'Orsik', 'Oskar', 'Rangrim', 'Rurik', 'Taklinn', 'Thoradin', 'Thorin', 'Tordek', 'Traubon', 'Travok', 'Ulfgar', 'Veit', 'Vondal'],
@@ -70,6 +120,64 @@ $(function () {
 	};
 
 	/**
+	 * Set NPC subrace
+	 * @return {string} The Merchant npc subrace
+	 */
+	var setSubrace = function(race) {
+
+		var subrace = '',
+				subraceRoll = roll(0, 100, 0);
+
+		switch(race) {
+			case 'Human':
+				if (subraceRoll <= 20) {
+					subrace = npc.subrace = 'Calishite';
+				}
+				else if (subraceRoll <= 30){
+					subrace = npc.subrace = 'Chondathan';
+				}
+				else if (subraceRoll <= 40){
+					subrace = npc.subrace = 'Damaran';
+				}
+				else if (subraceRoll <= 50){
+					subrace = npc.subrace = 'Illuskan';
+				}
+				else if (subraceRoll <= 60){
+					subrace = npc.subrace = 'Mulan';
+				}
+				else if (subraceRoll <= 70){
+					subrace = npc.subrace = 'Rashemi';
+				}
+				else if (subraceRoll <= 80){
+					subrace = npc.subrace = 'Shou';
+				}
+				else if (subraceRoll <= 90){
+					subrace = npc.subrace = 'Tethyrian';
+				}
+				else if (subraceRoll <= 100){
+					subrace = npc.subrace = 'Turami';
+				}
+			break;
+
+			case 'Dwarf':
+				console.log('2');
+			break;
+
+			case 'Halfling':
+				console.log('3');
+			break;
+
+			case 'Elf':
+				console.log('4');
+			break;
+		}
+
+		console.log(subrace);
+
+		return subrace;
+	};
+
+	/**
 	 * Set NPC race
 	 * 80% chance to generate a man
 	 * @return {string} The Merchant npc race
@@ -93,13 +201,35 @@ $(function () {
 			race = 'Elf';
 		}
 
+		setSubrace(race);
+
 		return race;
 	};
 
+	/**
+	 * Set NPC Name
+	 * @return {string} The Merchant npc name
+	 */
 	var setName = function(gender, race) {
-		var firstnames = nameList[race][gender],
-				lastnames = nameList[race]['lastnames'],
-				firstnamesLength = firstnames.length,
+
+		var firstnames,
+				lastnames;
+
+		if (typeof nameList[race]['subraces'] != 'undefined') {
+
+			var subrace = npc.subrace;
+
+			console.log(nameList[race]['subraces']['Mulan']['male']);
+			console.log(nameList[race]['subraces']['Mulan']['male']);
+
+			firstnames = nameList[race]['subraces'][npc.subrace][gender];
+			lastnames = nameList[race]['subraces'][npc.subrace]['lastnames'];
+		}
+
+			firstnames = nameList[race][gender];
+			lastnames = nameList[race]['lastnames'];
+
+		var	firstnamesLength = firstnames.length,
 				lastnamesLength = lastnames.length,
 				firstnameRoll = roll(0, firstnamesLength, 0),
 				lastnameRoll = roll(0, lastnamesLength, 0);
