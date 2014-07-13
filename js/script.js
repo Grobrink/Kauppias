98512273f6290e9700f162f6d03a1a52dd5aa17e
Pierre Lefèvre <karapost@gmail.com>
2014-07-13 20:45:41 +0200

    Clear script to use new Inkubator and Kauppias classes

--- js/script.js
+++ js/script.js
@@ -1,115 +1,11 @@
 $(function () {
 
 	// Import Inkubator
-	var inkubator = new Inkubator();
-	var npc = inkubator.getNpc();
+	var kauppias = new Kauppias();
+	var npc = kauppias.getNpc();
 
 	// Import Utils
 	var utils = new Utils();
-
-	var hierarchyReferences = [
-		'UpperCity',
-		'LowerCity',
-		'OuterCity',
-		'Street'
-	];
-
-	var hierarchy = {
-		'UpperCity': {
-			0: {
-				ratio: 10,
-				name: 'Master'
-			},
-			1: {
-				ratio: 40,
-				name: 'Companion'
-			},
-			2: {
-				ratio: 60,
-				name: 'Novice'
-			},
-			3: {
-				ratio: 70,
-				name: 'Seller'
-			},
-			4: {
-				ratio: 100,
-				name: 'Shipper'
-			}
-		},
-		'LowerCity': {
-			0: {
-				ratio: 20,
-				name: 'Master'
-			},
-			1: {
-				ratio: 60,
-				name: 'Companion'
-			},
-			2: {
-				ratio: 80,
-				name: 'Novice'
-			},
-			3: {
-				ratio: 100,
-				name: 'Shipper'
-			}
-		},
-		'OuterCity': {
-			0: {
-				ratio: 75,
-				name: 'Owner'
-			},
-			1: {
-				ratio: 85,
-				name: 'Seller'
-			},
-			2: {
-				ratio: 100,
-				name: 'Shipper'
-			}
-		},
-		'Street': {
-			0: {
-				ratio: 100,
-				name: 'Seller'
-			}
-		}
-	};
-
-
-	/**
-	 * Set npc hierarchy
-	 * @return {string} return the npc hierarchy in the shop
-	 */
-	var setHierarchy = function() {
-		var title,
-			hierarchyRoll = utils.roll(1, 100, 0);
-
-		// Get reference string
-		var referenceStr = hierarchyReferences[utils.roll(0, hierarchyReferences.length, 0)];
-
-		// Get neighbourhood properties
-		var neighbourhood = hierarchy[referenceStr];
-
-		// Get title
-		var index = 0,
-			length = Object.keys(neighbourhood).length,
-			title,
-			currentNeighbourhood;
-		for(index; index < length; index++) {
-
-			currentNeighbourhood = neighbourhood[index];
-			if (hierarchyRoll <= currentNeighbourhood.ratio) {
-
-				title = currentNeighbourhood.name;
-				break;
-			}
-		}
-
-		npc.hierarchy = title;
-		return title;
-	}
 
 	/**
 	 * Fill the html block
@@ -144,12 +40,12 @@ $(function () {
 		$('.cha').text(npc.attributes.cha);
 
 		// Set caracteristics modifier
-		$('.str-modifier').text(inkubator.getAttributeModifier(npc.attributes.str));
-		$('.dex-modifier').text(inkubator.getAttributeModifier(npc.attributes.dex));
-		$('.con-modifier').text(inkubator.getAttributeModifier(npc.attributes.con));
-		$('.int-modifier').text(inkubator.getAttributeModifier(npc.attributes.int));
-		$('.wis-modifier').text(inkubator.getAttributeModifier(npc.attributes.wis));
-		$('.cha-modifier').text(inkubator.getAttributeModifier(npc.attributes.cha));
+		$('.str-modifier').text(kauppias.getAttributeModifier(npc.attributes.str));
+		$('.dex-modifier').text(kauppias.getAttributeModifier(npc.attributes.dex));
+		$('.con-modifier').text(kauppias.getAttributeModifier(npc.attributes.con));
+		$('.int-modifier').text(kauppias.getAttributeModifier(npc.attributes.int));
+		$('.wis-modifier').text(kauppias.getAttributeModifier(npc.attributes.wis));
+		$('.cha-modifier').text(kauppias.getAttributeModifier(npc.attributes.cha));
 
 		// Set passive perception
 		$('.perception').text(npc.perception);
@@ -162,7 +58,7 @@ $(function () {
 
 		// Set hierarchy level
 		var descr = $('.description').text();
-		$('.description').text(descr + ', '+ setHierarchy());
+		$('.description').text(descr + ', '+ kauppias.setHierarchy());
 	};
 
 	fillBlock();
